7e7736ad43c1422608fac82960a69aed17e881dd
long1710 <37376007+long1710@users.noreply.github.com>
2019-12-03 01:57:26 -0600

    Add files via upload
    add process database

--- registration.js
+++ registration.js
@@ -5,9 +5,24 @@ function dbAccount(db, username, password){
     var sql = `INSERT INTO timetable.timetableproject (username, pass) VALUES ('${username}', '${password}')`;
     db.query(sql, function(err){
         if(err) throw err
-        console.log("succeed");
+        console.log("succeed in creating new account");
     })
     console.log(sql);
 }
 
-module.exports = dbAccount;
+//create username and password for the new account into database if account is register 
+function dbData(db, username, activities, priority){
+    var sql = `CREATE TABLE IF NOT EXISTS ${username} {
+            activities varCHAR(255),
+            priority INT
+    }`;
+    db.query(sql, function(err){
+        if(err) throw err
+        console.log('succeed in creating new table for account');
+    })
+    var sql1 = `INSERT INTO timetable.${username} (activities, priority) VALUES ('${activities}', '${priority}')`;
+}
+
+module.exports = {
+    dbAccount
+}
